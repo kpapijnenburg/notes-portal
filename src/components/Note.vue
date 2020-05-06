73196ce3d5566685bc937f3bec7a01eb764e1d33
= <=>
2020-05-06 15:15:05 +0200

    Finished detail view for note

--- src/components/Note.vue
+++ src/components/Note.vue
@@ -1,14 +1,28 @@
 <template>
   <div>
-    <div v-if="note">
-      <h1 class="md-display-1">{{this.note.title}}</h1>
-      <img src="https://ocr-demo.abtosoftware.com/uploads/handwritten2.jpg" alt />
-      <div v-if="handwriting">
-        <p v-for="sentence of sentences" :key="sentence.id">{{sentence}}</p>
-        <p>Status: {{this.handwriting.state}}</p>
+    <div v-if="note && handwriting">
+      <h1 class="md-headline">{{this.note.title}}</h1>
+      <div class="md-layout">
+        <p
+          class="md-caption md-layout-item md-size-20"
+        >Aangemaakt op: {{this.note.createdAt | localeDate}}</p>
+        <p
+          class="md-caption md-layout-item md-size-20"
+        >Laats aangepast op: {{this.note.updatedAt | localeDate}}</p>
+        <p class="md-caption md-layout-item md-size-20">Status: {{this.handwriting.state}}</p>
       </div>
-      <p>Aangemaakt op: {{this.note.createdAt | localeDate}}</p>
-      <p>Laats aangepast op: {{this.note.updatedAt | localeDate}}</p>
+      <md-card class="md-elevation-2">
+        <md-card-media>
+          <img src="https://via.placeholder.com/300x200" alt />
+        </md-card-media>
+      </md-card>
+      <div v-if="handwriting">
+        <p
+          class="md-display-1 md-layout md-alignment-center"
+          v-for="sentence of sentences"
+          :key="sentence.id"
+        >{{sentence}}</p>
+      </div>
     </div>
     <div v-if="!note">Selecteer een notitie...</div>
   </div>
@@ -43,6 +57,13 @@ export default {
     localeDate(value) {
       return new Date(value).toLocaleDateString("nl-NL");
     }
-  },
+  }
 };
 </script>
+
+<style scoped>
+.md-card {
+  margin: auto;
+  display: inline-block;
+}
+</style>
