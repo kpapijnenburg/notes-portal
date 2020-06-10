8ff4f3058c7b13b1d45f0449b22ac96d4b867215
= <=>
2020-06-10 10:31:31 +0200

    Added route protection

--- src/main.js
+++ src/main.js
@@ -12,12 +12,25 @@ import "vue-material/dist/vue-material.min.css";
 import "vue-material/dist/theme/default.css";
 
 const routes = [
-  { path: "/create", component: Create },
-  { path: "/view/:id", component: Note },
+  { path: "/create", component: Create, meta: { requiresAuth: true } },
+  { path: "/view/:id", component: Note, meta: { requiresAuth: true } },
   { path: "/login", component: Login },
+  // Redirect to home
+  { path: "*", redirect: "/" },
 ];
 
 const router = new VueRouter({ routes });
+
+router.beforeEach((to, from, next) => {
+  const loggedIn = localStorage.getItem("user");
+  const authRequired = to.matched.some((record) => record.meta.requiresAuth);
+
+  if (authRequired && !loggedIn) {
+    return next("/login");
+  }
+
+  next();
+});
 
 Vue.use(VueMaterial);
 Vue.use(VueRouter);
