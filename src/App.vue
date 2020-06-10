cd1c7e1fdcddac5b4f4d1deb1b0d1efacb46071d
= <=>
2020-06-10 09:34:52 +0200

    Finished login component

--- src/App.vue
+++ src/App.vue
@@ -7,26 +7,28 @@
           <md-icon class="fa fa-user"></md-icon>
         </md-button>
       </md-app-toolbar>
-      <md-app-drawer md-permanent="card">
-        <md-toolbar class="md-transparent md-title" md-elevation="0">Mijn Notities</md-toolbar>
-        <md-list>
-          <md-divider></md-divider>
-          <md-list-item
-            v-for="note of $store.state.notes"
-            :to="`/view/${note.id}`"
-            :key="note.id"
-          >{{note.title}}</md-list-item>
-          <md-divider></md-divider>
-          <md-list-item>
-            <md-button
-              to="/create"
-              class="md-icon-button md-dense md-raised md-primary md-alignment-right"
-            >
-              <md-icon>+</md-icon>
-            </md-button>
-          </md-list-item>
-        </md-list>
-      </md-app-drawer>
+      <div v-if="this.loggedIn">
+        <md-app-drawer md-permanent="card">
+          <md-toolbar class="md-transparent md-title" md-elevation="0">Mijn Notities</md-toolbar>
+          <md-list>
+            <md-divider></md-divider>
+            <md-list-item
+              v-for="note of $store.state.notes"
+              :to="`/view/${note.id}`"
+              :key="note.id"
+            >{{note.title}}</md-list-item>
+            <md-divider></md-divider>
+            <md-list-item>
+              <md-button
+                to="/create"
+                class="md-icon-button md-dense md-raised md-primary md-alignment-right"
+              >
+                <md-icon>+</md-icon>
+              </md-button>
+            </md-list-item>
+          </md-list>
+        </md-app-drawer>
+      </div>
       <md-app-content>
         <router-view></router-view>
       </md-app-content>
@@ -41,13 +43,9 @@ export default {
   name: "App",
   data: () => {
     return {
-      service: new NotesService()
+      service: new NotesService(),
+      loggedIn: false
     };
-  },
-  methods: {
-    handleLoginClick() {
-      console.log("Login click");
-    }
   },
   async mounted() {
     this.$store.state.notes = await this.service.Get();
--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,13 +1,61 @@
 <template>
-  <h1>LOOOOOOGIIIIIIIIIIIIIIIIIIIIIIn</h1>
+  <div>
+    <h2>Login</h2>
+    <form @submit.prevent="handleSubmit">
+      <md-field :class="{ 'md-invalid': isInvalid }">
+        <label for="email">Email adres</label>
+        <md-input type="text" v-model="email" name="email"></md-input>
+      </md-field>
+        <span v-show="submitted && !email" class="invalid-feedback">Emailadres is benodigd</span>
+      <md-field>
+        <label for="password">Wachtwoord</label>
+        <md-input type="password" v-model="password" name="password"></md-input>
+      </md-field>
+        <div v-show="submitted && !password" class="invalid-feedback">Wachtwoord is benodigd</div>
+      <md-button type="submit" class="md-raised md-primary" :disabled="loggingIn">Inloggen</md-button>
+      <img
+        v-show="loggingIn"
+        src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA=="
+      />
+    </form>
+  </div>
 </template>
 
 <script>
 export default {
-    
-}
+  data() {
+    return {
+      email: "",
+      password: "",
+      submitted: false
+    };
+  },
+  computed: {
+    logginIn() {
+      return this.$store.state.authentication.status.loggingIn;
+    }
+  },
+  created() {
+    // reset login status
+    this.$store.dispatch("authentication/logout");
+  },
+  methods: {
+    handleSubmit(e) {
+      console.log("Submitting", e);
+      this.submitted = true;
+      const { email, password } = this;
+      const { dispatch } = this.$store;
+
+      if (email && password) {
+        dispatch("authentication/login", { email, password });
+      }
+    }
+  }
+};
 </script>
 
 <style>
-
+  .invalid-feedback {
+    color: red
+  }
 </style>
